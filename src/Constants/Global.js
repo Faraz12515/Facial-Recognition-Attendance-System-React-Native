313ee7f953984c1f0e1a906ac899fddc23000fd2
Noor Ahmed <nq73399@gmail.com>
2021-02-24 21:45:04 +0500

    Something changed

--- src/Constants/Global.js
+++ src/Constants/Global.js
@@ -1,4 +1,4 @@
-export const END_POINT = 'https://789e3d5add24.ngrok.io';
+export const END_POINT = 'https://f26b45af275c.ngrok.io';
 // export const END_POINT = 'http://127.0.0.1:5000/';
 export const SIGN_UP = END_POINT + '/user_signup/';
 export const LOGIN = END_POINT + '/login/';
